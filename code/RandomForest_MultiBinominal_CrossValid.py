f002b05f392866932d4fef12e7f7197e48a2dab5
nanfeisun98 <111715016+nanfeisun98@users.noreply.github.com>
2024-06-09 11:50:15 -0500

    Upload new code with confusion matrix code
    Added new function to find the best f1_score data point on AUC curve, and print out its confusion matrix

--- code/RandomForest_MultiBinominal_CrossValid.py
+++ code/RandomForest_MultiBinominal_CrossValid.py
@@ -14,10 +14,27 @@ import numpy as np
 import pandas as pd
 
 # Initialize with whatever parameters you want to
-clf = RandomForestClassifier()
+clf = RandomForestClassifier(n_estimators=100, random_state=42)
 
 csvname = 'featuresNew_12_cross.csv'
-# csvname = 'caseNew_5_cross.csv'
+# # csvname = '..\\DEAP_Dataset\\featuresNew_10_cross_new.csv' #combine arousal and valence
+# # csvname = '..\\DEAP_Dataset\\featuresNew_11_cross_new_arousal.csv' #arousal only, 3.3, 6.67
+# # csvname = '..\\DEAP_Dataset\\featuresNew_11_cross_new_dominance.csv' #arousal only, 3.3, 6.67
+# # csvname = '..\\DEAP_Dataset\\featuresNew_11_cross_new_liking.csv' #arousal only, 3.3, 6.67
+# # csvname = '..\\DEAP_Dataset\\featuresNew_11_cross_new_valence.csv' #arousal only, 3.3, 6.67
+# csvname = '..\\DEAP_Dataset\\featuresNew_11_cross_new_arousal_7_3.csv' #arousal 5
+# # csvname = '..\\DEAP_Dataset\\featuresNew_11_cross_new_arousal_5.csv' #arousal 5
+# # csvname = '..\\DEAP_Dataset\\featuresNew_13_cross_clustermarker.csv' #arousal 5
+# # csvname = '..\\DEAP_Dataset\\featuresNew_14_cross_clustermarker.csv' #arousal 5* 0.31 - valence * 0.1 - 0 .2
+# # csvname = '..\\DEAP_Dataset\\featuresNew_16_cross_labels.csv'
+# # csvname = '..\\DEAP_Dataset\\featuresNew_16_cross_labels_by_video1.csv'
+# csvname = '..\\case_Dataset\\caseNew_1_cross_NO_3.csv' #arousal 5
+# csvname = '..\\case_Dataset\\caseNew_2_cross.csv' #arousal 5
+# # csvname = '..\\case_Dataset\\caseNew_3_cross.csv'
+# # csvname = '..\\case_Dataset\\caseNew_4_cross.csv'
+# csvname = '..\\case_Dataset\\new\\caseNew_5_cross_1221.csv'
+# csvname = '..\\..\\data_NoPMRB\\featuresNew_9_cross.csv'
+csvname = '..\\..\\data_NoPMRB\\featuresNew_12_cross_Clean.csv'
 isNewFile = False
 if 'cross' in csvname:
     isNewFile = True
@@ -49,15 +66,51 @@ data_final=data[to_keep]
 #include only selected columns
 X = data_final.loc[:, data_final.columns != targetCol]
 y = data_final.loc[:, data_final.columns == targetCol]
-print(X.head())
-print(y.head())
+# print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+# print('X length', len(X))
 
-cv = ShuffleSplit(n_splits=3, test_size=0.2, random_state=0)
+# from imblearn import under_sampling, over_sampling
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import train_test_split
+
+def over_sample(data_final):
+    #*******over sample the NO data records, so the yes and no samples *******
+    # are having balanced numbers
+    X = data_final.loc[:, data_final.columns != targetCol]
+    y = data_final.loc[:, data_final.columns == targetCol]
+    #PYTHON 3.8
+    #pip install -U imbalanced-learn
+    #conda install -c conda-forge imbalanced-learn
+
+    os = SMOTE(random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+    # print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+    # print('X_train size:', len(X_train), 'X_test size:', len(X_test))
+    columns = X_train.columns
+    os_data_X,os_data_y=os.fit_resample(X_train, y_train)
+    os_data_X = pd.DataFrame(data=os_data_X,columns=columns )
+    os_data_y= pd.DataFrame(data=os_data_y,columns=[targetCol])
+    # we can Check the numbers of our data
+    print("length of oversampled data is ",len(os_data_X))
+    print("Number of no subscription in oversampled data",len(os_data_y[os_data_y[targetCol]==0]))
+    print("Number of subscription",len(os_data_y[os_data_y[targetCol]==1]))
+    print("Proportion of no subscription data in oversampled data is ",len(os_data_y[os_data_y[targetCol]==0])/len(os_data_X))
+    print("Proportion of subscription data in oversampled data is ",len(os_data_y[os_data_y[targetCol]==1])/len(os_data_X))
+    
+    return os_data_X, os_data_y
+
+# oversample
+os_data_X, os_data_y = over_sample(data_final)
+X=os_data_X
+y=os_data_y
+
+# print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+# print('oversampled X length', len(X))
+
+cv = ShuffleSplit(n_splits=3, test_size=0.33, random_state=0)
 results_accuracy = cross_val_score(clf, X, y.values.ravel(), cv=cv)
 scoring = ['precision_macro', 'recall_macro', 'f1_macro']
 scores = cross_validate(clf, X, y.values.ravel(), cv=cv, scoring=scoring)
-
-
 
 print('#####random forest results#####')
 print(results_accuracy)
@@ -78,17 +131,6 @@ print('##### DCTree results#####')
 print(tree_results)
 print(np.mean(tree_results))
 
-#SVM
-# from sklearn import svm
-# #Create a svm Classifier
-# svm_clf = svm.SVC(kernel='linear')
-# svm_results = cross_val_score(svm_clf, X_train, y_train.values.ravel(), cv=cv)
-
-# print('##### SVM results#####')
-# print(svm_results)
-# # 10-Fold Cross validation
-# print(np.mean(svm_results))
-
 #prepare multi-binominal
 y = y.values.ravel()
 myY1 = deepcopy(y)
@@ -96,7 +138,7 @@ myY2 = deepcopy(y)
 myY3 = deepcopy(y)
 myY=[myY1,myY2,myY3]
 
-#0 is PR, 1 is ST, 2 is DT
+#0 is PR, 1 is ST, 2 is LT
 count_1 = 0
 count_0 = 0
 for i in range(len(myY[0])):
@@ -132,34 +174,88 @@ for i in range(len(myY[2])):
     else:
         myY[2][i] = 0
         count_0+=1
-print('DT 1 count is:', count_1)
-print('DT 0 count is:', count_0)
+print('LT 1 count is:', count_1)
+print('LT 0 count is:', count_0)
 
 from sklearn.model_selection import RandomizedSearchCV, train_test_split
 #from scipy.stats import randint
 # Split the data into training and test sets
 # predict PR (0)
 
-sessions = ['PR', 'ST', 'DT']
+sessions = ['PR', 'ST', 'LT']
 
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
-from sklearn.metrics import RocCurveDisplay, roc_curve
+from sklearn.metrics import RocCurveDisplay, roc_curve, precision_recall_curve
 import matplotlib.pyplot as plt
 from sklearn.metrics import auc
 
+def find_best_f1_threshold(y_test, y_score):
+    max_f1 = 0
+    best_threshold = 0
+    precision, recall, threshold = precision_recall_curve(y_test, y_score, pos_label=1)
+    for fpr_item, tpr_item, threshold_item in zip(precision, recall, threshold):
+        if tpr_item+fpr_item == 0:
+            continue
+        f1 = 2*(tpr_item*fpr_item)/(tpr_item+fpr_item)
+        # print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+        # print('precision', fpr_item, 'recall', tpr_item,'f1',f1)
+        if f1 > max_f1:
+            max_f1 = f1
+            best_threshold = threshold_item
+        # print('f1=',f1, "{:.2f}".format(fpr_item), "{:.2f}".format(tpr_item), "{:.2f}".format(threshold_item))
+        
+    fpr, tpr, threshold = roc_curve(y_test, y_score, pos_label=1)
+    best_tpr = best_fpr = 0
+    threhold_min_diff = 100.0
+    
+    for fpr_item, tpr_item, threshold_item in zip(fpr, tpr, threshold):
+        threhold_diff = abs(threshold_item - best_threshold)
+        if threhold_diff < threhold_min_diff:
+            threhold_min_diff = threhold_diff
+            best_tpr = tpr_item
+            best_fpr = fpr_item
+            # print('fpr:', "{:.4f}".format(fpr_item),'tpr', "{:.4f}".format(tpr_item), "{:.2f}".format(threshold_item))
+    
+    y_test_last_col = y_test[:].tolist()
+    print(len(y_test_last_col),y_test_last_col.count(0),y_test_last_col.count(1))
+    P = y_test_last_col.count(1) #P=TP+FN
+    N = y_test_last_col.count(0) #N=TN+FP
+    
+    TP = best_tpr*P
+    FP = best_fpr*N
+    TN = N - FP
+    FN = P - TP
+    
+    return TP,FP,TN,FN, best_threshold
+
 def plot_feature_importance(forest):
+    figureName = 'RandomForest_OfficeTask_feat_importance'
     importances = forest.feature_importances_
     std = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
     forest_importances = pd.Series(importances, index=feature_names)
     forest_importances_err = pd.Series(std, index=feature_names)
-    print(forest_importances)
-    print(forest_importances_err)
+    x = forest_importances.nlargest(8)
+    print(x)
+    # print(forest_importances_err)
+    
+    # fig, ax = plt.subplots()
+    # forest_importances.plot.bar(yerr=std, ax=ax)
+    # ax.set_title("Feature importances using MDI")
+    # ax.set_ylabel("Mean decrease in impurity")
+    # fig.tight_layout()
+    # plt.grid(False)    
+    
+    # print(figureName+' saved!')
+    # plt.savefig(figureName, format="png",dpi=600)
+    # plt.show()
 
-def rand_cross(y, sessionString, fig, ax, treatment):
+def rand_cross(y, sessionString, fig, ax, treatment_index):
+    colorMean = ["r","g","b"]
+    treatments = ['PR','ST','LT']
     tprs = []
     aucs = []
     mean_fpr = np.linspace(0, 1, 100)
-    rf = RandomForestClassifier()
+    rf = RandomForestClassifier(n_estimators=100, random_state=0)
     # fig, ax = plt.subplots()
     for i, (train, test) in enumerate(cv.split(X, y)):
         X_train = X.values[train]
@@ -179,20 +275,33 @@ def rand_cross(y, sessionString, fig, ax, treatment):
         
         my_X_test = X.values[test]
         my_Y_test = y[test]
+        print(treatments[treatment_index], '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+        # print(treatments[treatment_index],'x_train size:', len(X_train), 'x_test size:', len(my_X_test))
         y_pred = rf.predict(my_X_test) 
         accuracy = accuracy_score(my_Y_test, y_pred)
         precision = precision_score(my_Y_test, y_pred)
         recall = recall_score(my_Y_test, y_pred)
         f1_score = (2 * precision * recall) / (precision + recall)
-        print("Accuracy:", accuracy, 'Precision', precision, 'Recall',recall, 'f1 score', f1_score)
+        # print("Accuracy:", accuracy, 'Precision', precision, 'Recall',recall, 'f1 score', f1_score)
+        
+        y_score = rf.predict_proba(my_X_test)[:,1]
+        # print('length of y_score', len(y_score))
+        # print('y_score',y_score)
+        TP,FP,TN,FN, best_threshold = find_best_f1_threshold(my_Y_test, y_score)
+        print('TP',TP,'FP',FP,'TN',TN,'FN',FN,'best threshold', best_threshold)
+        recall = TP/(TP+FN)
+        precision = TP/(TP+FP)
+        accuracy = (TP+TN)/(TP+TN+FP+FN)
+        f1 = 2*precision*recall/(precision+recall)
+        print(treatments[treatment_index],'recall',recall,'precision',precision,'accuracy',accuracy,'f1',f1)
+        
         
         interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
         interp_tpr[0] = 0.0
         tprs.append(interp_tpr)
         aucs.append(viz.roc_auc)
         
-    colorMean = ["r","g","b"]
-    treatments = ['PR','ST','LT']
+    
     mean_tpr = np.mean(tprs, axis=0)
     mean_tpr[-1] = 1.0
     mean_auc = auc(mean_fpr, mean_tpr)
@@ -200,8 +309,8 @@ def rand_cross(y, sessionString, fig, ax, treatment):
     ax.plot(
         mean_fpr,
         mean_tpr,
-        color=colorMean[treatment],
-        label=r"%s Mean ROC (AUC = %0.2f $\pm$ %0.2f)" % (treatments[treatment], mean_auc, std_auc),
+        color=colorMean[treatment_index],
+        label=r"%s Mean ROC (AUC = %0.2f $\pm$ %0.2f)" % (treatments[treatment_index], mean_auc, std_auc),
         lw=2,
         alpha=0.8,
     )
@@ -210,12 +319,12 @@ def rand_cross(y, sessionString, fig, ax, treatment):
     tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
     tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
 
-    if treatment == 0:
+    if treatment_index == 0:
         ax.fill_between(
             mean_fpr,
             tprs_lower,
             tprs_upper,
-            color=colorMean[treatment],
+            color=colorMean[treatment_index],
             alpha=0.2,
             label=r"$\pm$ 1 std. dev.",
         )
@@ -224,7 +333,7 @@ def rand_cross(y, sessionString, fig, ax, treatment):
             mean_fpr,
             tprs_lower,
             tprs_upper,
-            color=colorMean[treatment],
+            color=colorMean[treatment_index],
             alpha=0.2,
         )
         
@@ -233,8 +342,8 @@ def rand_cross(y, sessionString, fig, ax, treatment):
 
 
 def rand_predict(y, sessionString):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
-    rf = RandomForestClassifier()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, randon_state=0)
+    rf = RandomForestClassifier(n_estimators=100, random_state=42)
     rf.fit(X_train, y_train)
     y_pred = rf.predict(X_test)
     
@@ -244,113 +353,25 @@ def rand_predict(y, sessionString):
     recall = recall_score(y_test, y_pred)
     f1_score = (2 * precision * recall) / (precision + recall)
     print(sessionString, "Accuracy:", accuracy, 'Precision', precision, 'Recall',recall, 'f1 score', f1_score)
+    cm = confusion_matrix(y_test, y_pred)
+    print(cm)
+    tn, fp, fn, tp = cm.ravel()
+    print('TP:', tp, 'TN:',tn, 'FP:',fp,'FN:',fn)
     
-    rfc = RandomForestClassifier(n_estimators=10, random_state=42)
+    rfc = RandomForestClassifier(n_estimators=100, random_state=42)
     rfc.fit(X_train, y_train)
     ax = plt.gca()
-    rfc_disp = RocCurveDisplay.from_estimator(rfc, X_test, y_test, ax=ax, alpha=0.8)
+    # rfc_disp = RocCurveDisplay.from_estimator(rfc, X_test, y_test, ax=ax, alpha=0.8)
+    # svc_disp.plot(ax=ax, alpha=0.8)
+    # plt.show()
+    
+    #print out feature importance
+   #  (pd.Series(rfc.feature_importances_, index=X.columns)
+   # .nlargest(6)
+   # .plot(kind='barh'))  
 
     return accuracy
 
-#adaboost
-from sklearn.ensemble import AdaBoostClassifier
-
-def ada_cross(y, sessionString, fig, ax, treatment):
-    tprs = []
-    aucs = []
-    mean_fpr = np.linspace(0, 1, 100)
-    ada_clf = AdaBoostClassifier(n_estimators=100)
-    # fig, ax = plt.subplots()
-    for i, (train, test) in enumerate(cv.split(X, y)):
-        X_train = X.values[train]
-        y_train = y[train]
-        ada_clf.fit(X_train, y_train)
-        viz = RocCurveDisplay.from_estimator(
-            ada_clf,
-            X.values[test],
-            y[test],
-            name="ROC fold {}".format(i),
-            alpha=0.0,
-            lw=1,
-            ax=ax,
-        )
-        
-        my_X_test = X.values[test]
-        my_Y_test = y[test]
-        y_pred = ada_clf.predict(my_X_test) 
-        accuracy = accuracy_score(my_Y_test, y_pred)
-        precision = precision_score(my_Y_test, y_pred)
-        recall = recall_score(my_Y_test, y_pred)
-        f1_score = (2 * precision * recall) / (precision + recall)
-        print("Accuracy:", accuracy, 'Precision', precision, 'Recall',recall, 'f1 score', f1_score)
-        
-        interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
-        interp_tpr[0] = 0.0
-        tprs.append(interp_tpr)
-        aucs.append(viz.roc_auc)
-        
-    colorMean = ["r","g","b"]
-    treatments = ['PR','ST','LT']
-    mean_tpr = np.mean(tprs, axis=0)
-    mean_tpr[-1] = 1.0
-    mean_auc = auc(mean_fpr, mean_tpr)
-    std_auc = np.std(aucs)
-    ax.plot(
-        mean_fpr,
-        mean_tpr,
-        color=colorMean[treatment],
-        label=r"%s Mean ROC (AUC = %0.2f $\pm$ %0.2f)" % (treatments[treatment], mean_auc, std_auc),
-        lw=2,
-        alpha=0.8,
-    )
-    
-    std_tpr = np.std(tprs, axis=0)
-    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
-    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-
-    if treatment == 0:
-        ax.fill_between(
-            mean_fpr,
-            tprs_lower,
-            tprs_upper,
-            color=colorMean[treatment],
-            alpha=0.2,
-            label=r"$\pm$ 1 std. dev.",
-        )
-    else:
-        ax.fill_between(
-            mean_fpr,
-            tprs_lower,
-            tprs_upper,
-            color=colorMean[treatment],
-            alpha=0.2,
-        )
-        
-    
-    return accuracy, mean_auc
-    
-def ada_predict(y, sessionString):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    ada_clf = AdaBoostClassifier(n_estimators=100)
-    
-    ada_clf.fit(X_train, y_train)
-    y_pred = ada_clf.predict(X_test)
-    
-    
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
-    f1_score = (2 * precision * recall) / (precision + recall)
-    print(sessionString, "Accuracy:", accuracy, 'Precision', precision, 'Recall',recall, 'f1 score', f1_score)
-    
-    ax = plt.gca()
-    ada_disp = RocCurveDisplay.from_estimator(ada_clf, X_test, y_test, ax=ax, alpha=0.8)
-    
-    # scores = cross_val_score(ada_clf, X, y, cv=5)
-    # accuracy = scores.mean()    
-    # print(sessionString, "Accuracy:", accuracy)
-    return accuracy
-   
 flag = True 
 while flag:
     # flag = False
@@ -364,6 +385,7 @@ while flag:
         accuracy_results.append(accuracy)
         auc_results.append(mean_auc)
         # accuracy_results.append(rand_predict(myY[i],sessions[i]))
+        # rand_predict(myY[i],sessions[i])
     
     #draw all elements for merged figures
     ax.plot([0, 1], [0, 1], linestyle="--", lw=2, color="black", label="Chance", alpha=0.8)
@@ -397,17 +419,10 @@ while flag:
     auc_mean = np.mean(auc_results) 
     print('accuracy',accuracy_mean)
     print('auc',auc_mean)
-    
-    
-    #skip adaboost
-    # print('##########   adaboost results:    ##########')
-    # ada_accuracy_results = []
-    # for i in range(3):
-    #     ada_accuracy_results.append(ada_predict(myY[i],sessions[i]))
-        
+            
     plt.show()
     print(accuracy_mean)
-    
+    break
     if auc_mean > 0.635:
         break
 
